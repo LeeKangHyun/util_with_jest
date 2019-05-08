b564fd6c2cfd70fe6166fc24bc2085d656017f40
이강현 <khlee92@station3.co.kr>
2019-05-08 17:22:08 +0900

    canvas 연습 중

--- src/pages/Canvas/Components/ColorPicker/index.js
+++ src/pages/Canvas/Components/ColorPicker/index.js
@@ -1,6 +1,18 @@
 import React, { useRef, useCallback, useState, useLayoutEffect } from 'react'
+import styled from 'styled-components'
+
+const Wrap = styled.div`
+  width: 100%;
+`
+
+const CanvasComponent = styled.canvas`
+  width: ${props => props.width}px;
+  height: ${props => props.height}px;
+`
 
 export default () => {
+  const [width, setWidth] = useState(300)
+  const [height, setHeight] = useState(150)
   const [ctx, setCtx] = useState(null)
   const img = new Image()
   
@@ -16,6 +28,7 @@ export default () => {
     const color = Color.current
     let x = event.layerX
     let y = event.layerY
+    console.log(x, y)
     let pixel = ctx.getImageData(x, y, 1, 1)
     let data = pixel.data
     let rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`
@@ -38,17 +51,31 @@ export default () => {
   useLayoutEffect(() => {
     if (ctx) {
       img.onload = function() {
+        const {width, height} = img
+        setWidth(width)
+        setHeight(height)
+        /*const canvas = ctx.canvas;
+        let hRatio = canvas.width / img.width
+        let vRatio = canvas.height / img.height
+        let ratio = Math.min(hRatio, vRatio)
+        
+        let centerShift_x = (canvas.width - img.width * ratio) / 2
+        let centerShift_y = (canvas.height - img.height * ratio) / 2
+        
+        ctx.clearRect(0, 0, img.width, img.height)
+        
+        ctx.drawImage(img, 0, 0, img.width, img.height, centerShift_x, centerShift_y, img.width * ratio, img.height * ratio)*/
         ctx.drawImage(img, 0, 0)
-        img.style.display = 'none'
+        // img.style.display = 'none'
       }
     }
   }, [Canvas, img, ctx])
   
   
   return (
-    <div>
-      <canvas ref={Canvas}></canvas>
+    <Wrap>
+      <CanvasComponent ref={Canvas} width={width} height={height}></CanvasComponent>
       <div ref={Color} />
-    </div>
+    </Wrap>
   )
 }
